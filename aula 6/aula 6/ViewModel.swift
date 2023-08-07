62dbb2772b06f471b8483e456a82bc0dda37cf33
Lorenzo <72982330+lorenzo-burgos@users.noreply.github.com>
2023-08-07 20:41:35 -0300

    Add files via upload

--- aula 6/aula 6/ViewModel.swift
+++ aula 6/aula 6/ViewModel.swift
@@ -13,11 +13,35 @@ struct ViewModel: View {
     var body: some View {
         VStack {
             if let bard = viewModel.bard {
+                Text("D&DPedia")
                 Text(viewModel.bard!.name)
+                Spacer()
                 Text("Hit Die: D\(viewModel.bard!.hit_die)")
-                Text("Proficiency Choices:")
-                ForEach(viewModel.bard!.proficiency_choices, id: \.self) { choice in
-                    Text(choice.desc)
+                Spacer()
+                VStack{
+                    Text("Tool Proeficiency")
+                    ForEach(viewModel.bard!.proficiency_choices, id: \.self) { choice in
+                        Text(choice.desc)
+                    }
+                    Spacer()
+                    Text("Weapons Proficiencies:")
+                    ForEach(viewModel.bard!.proficiencies.prefix(6), id: \.self) { proficiency in
+                        if let proficiencyName = proficiency.name {
+                            Text(proficiencyName)
+                        }
+                    }
+                    VStack{
+                        Spacer()
+                        Text("Saving Throws")
+                        ForEach(viewModel.bard!.saving_throws, id: \.self){savingthrow in
+                            Text(savingthrow.name)
+                        }
+                        Spacer()
+                        Text("Starting equipment:")
+                        ForEach(viewModel.bard!.starting_equipment, id: \.self){ startingequipment in
+                            Text(startingequipment.equipment.name!)
+                        }
+                    }
                 }
             } else {
                 ProgressView()
